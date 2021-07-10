43e73ac78fe584ced1029c60142cab7d6893b494
0hyeon <65083089+0hyeon@users.noreply.github.com>
2021-07-10 22:20:35 +0900

    210710

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@
 import React , { useState }from "react"; //이코드는 리액트를 불러와서 사용할수있게해주는 코드
 // import logo from "./logo.svg";
 import "./App.css";
-import Counter from './8.3.1_useReducer'; //import 함수 from 파일이름
+import Average from './8.4.2_useMemo'; //import 함수 from 파일이름
 const App = () => {
   // const [visible, setVisible] = useState(false);
-  return <Counter />;
+  return <Average />;
 };
 
 export default App;

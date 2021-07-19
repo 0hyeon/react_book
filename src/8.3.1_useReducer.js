ce30b8f46e4154543f0bbf2e85da83253b386a4f
0hyeon <65083089+0hyeon@users.noreply.github.com>
2021-07-20 00:15:32 +0900

    210720

--- src/8.3.1_useReducer.js
+++ src/8.3.1_useReducer.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react'
 
 function reducer(state,action){
-    //action.type에 따라 달ㄴ 작업 수행
+    //action.type에 따라 다른 작업 수행
     switch (action.type){
         case 'INCREMENT':
             return {value:state.value + 1};
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React , { useState }from "react"; //이코드는 리액트를 불러와서 사용할수있게해주는 코드
 // import logo from "./logo.svg";
 import "./App.css";
-import Average from './8.4.2_useMemo'; //import 함수 from 파일이름
+import Average from './8.5_useCallback'; //import 함수 from 파일이름
 const App = () => {
   // const [visible, setVisible] = useState(false);
   return <Average />;

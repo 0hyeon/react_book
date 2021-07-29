5365c0828c63375796d34d159b782df4b03afb47
0hyeon <65083089+0hyeon@users.noreply.github.com>
2021-07-29 18:32:36 +0900

    210729

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@
 import React , { useState }from "react"; //이코드는 리액트를 불러와서 사용할수있게해주는 코드
 // import logo from "./logo.svg";
 import "./App.css";
-import Average from './8.5_useCallback'; //import 함수 from 파일이름
+import SassComponent from './9.2_SassComponent'; //import 함수 from 파일이름
 const App = () => {
   // const [visible, setVisible] = useState(false);
-  return <Average />;
+  return <SassComponent />;
 };
 
 export default App;
